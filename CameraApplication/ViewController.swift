442e09836418443ea139d9bf7b1043f311d68a75
Ritika Verma <ritika.v2000@gmail.com>
2021-09-06 14:20:10 +0530

    Initial Commit

---
+++ CameraApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApplication
+//
+//  Created by RITIKA VERMA on 06/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
